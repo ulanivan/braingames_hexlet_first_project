723e949a764becfcbda152933e0554bec6e3085f
Ivan Ulanovich <ulanivan@yandex.ru>
2019-04-08 17:38:56 +0500

    error correction

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,7 @@ module.exports = {
     presets: [
       ['@babel/env', {
         targets: {
-          node: 'current',
-          firefox: '60',
-          chrome: '67',
-          safari: '11.1',
+          node: 'current'
         },
       }],
     ],
