70aacd4414159b470fd5245efc20167f05d7ea44
Ivan Ulanovich <ulanivan@yandex.ru>
2019-04-09 00:21:30 +0500

    second task

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+const welcomeText = 'Welcome to the Brain Games!\n';
+console.log(welcomeText);
+
+const nameQuestion = readlineSync.question('May I have your name? ');
+
+console.log(`Hello, ${nameQuestion}!`);
+
+export default nameQuestion;
