48a01dad655fc479e53c492bdb948f5b8e6882e2
Ivan Ulanovich <ulanivan@yandex.ru>
2019-04-09 14:46:51 +0500

    error correction 2lvl

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,4 @@
 #!/usr/bin/env node
 import nameQuestion from '..';
+
+console.log(`Hello, ${nameQuestion}!`);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const welcomeText = 'Welcome to the Brain Games!\n';
+const welcomeText = 'Welcome to the Brain Games!\n'; 
 console.log(welcomeText);
-
 const nameQuestion = readlineSync.question('May I have your name? ');
-
-console.log(`Hello, ${nameQuestion}!`);
 
 export default nameQuestion;
